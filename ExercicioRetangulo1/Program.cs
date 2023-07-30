0d1653b6d76cb286982dd2bcda4f0ed817e5b769
Henrique Mendes <henriquemendes13@gmail.com>
2023-07-30 20:18:02 -0300

    Add project files.

---
+++ ExercicioRetangulo1/Program.cs
@@ -0,0 +1,21 @@
+﻿double bse, altura, area, perimetro, diagonal;
+
+Console.WriteLine("BASE DO RETANGULO: ");
+bse = double.Parse(Console.ReadLine());
+
+Console.WriteLine("ALTURA DO RETANGULO: ");
+altura = double.Parse(Console.ReadLine());
+
+
+area = bse * altura;
+perimetro = (bse * 2) + (altura * 2);
+diagonal = Math.Sqrt(bse * bse + altura * altura);
+
+
+Console.WriteLine("AREA DO RETANGULO: " + area.ToString("f4"));
+
+
+Console.WriteLine("PERIMETRO DO RETANGULO: " + perimetro.ToString("F4"));
+
+
+Console.WriteLine("DIAGOAL DO RETANGULO: " + diagonal.ToString("f4"));
